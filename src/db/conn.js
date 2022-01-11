215a2c18fdb0bc6a77d6984e1c21683b94f78703
riyaGurbani <riya990.rg85@gmail.com>
2022-01-11 17:09:30 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,16 @@
 const mongoose =require("mongoose");
 
 // ------------databse connection----------------
-mongoose.connect("")
+mongoose.connect("mongodb+srv://ngo-user:ngouser@cluster0.pfbcq.mongodb.net/NGO?retryWrites=true&w=majority", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+   // useCreateIndex:true
+
+}).then(() => {
+    console.log(`connection successful`);
+
+}).catch((e) => {
+    console.log(e);
+
+})
+
